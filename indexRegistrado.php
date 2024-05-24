4273b913c0b149d67127e85a180048d99f46a37d
Juan <jestebanl88m@gmail.com>
2024-05-24 13:11:39 +0200

    arreglado sticky

--- indexRegistrado.php
+++ indexRegistrado.php
@@ -18,7 +18,8 @@ $variable_php = 'valor';
     <title>Index Registrado</title>
     <link rel="stylesheet" href="./styles/indexRegistrado.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="./styles/scrollbar.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css?v=<?php echo time(); ?>">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="./logicaGeneral/llegadaDatosPerfiles.js?v=<?php echo time(); ?>" defer></script>
     </script>
@@ -34,29 +35,30 @@ $variable_php = 'valor';
             <img src="img/iconoUser.png" alt="">
         </div>
     </header>
-    <main id="cuerpo">
-        <aside id="menuLat">
-            <div class="menuLateral">
-                <ul>
-                    <!-- <li><a href="#"><i class="fas fa-tv"></i>dashboard</a></li> -->
-                    <li class="opL"><a href="#"><i class='fa-solid fa-house'></i></a></li>
-                    <li class="opL"><a href="#"><i class="far fa-user"></i></a></li>
-                    <!-- <li><a href="#"><i class="fab fa-gripfire"></i>trending</a></li> -->
-                    <li class="opL"><a href="#"><i class="far fa-comments"></i></a></li>
-                    <!-- <li><a href="#"><i class="far fa-folder"></i>file manager</a></li> -->
-                    <!-- <li><a href="#"><i class="far fa-address-book"></i>portfolio</a></li> -->
-                    <li class="opL"><a href="#"><i class="fas fa-heart"></i></a></li>
-                    <li class="opL"><a href="#"><i class="fas fa-cogs"></i></a></li>
-                    <li class="opL"><a href="./inicioSesionForm/cerrarSesion.php"><i class='fas fa-sign-out-alt'></i></a></li>
-                </ul>
-            </div>
-        </aside>
-        <section id="resultadosPerfiles">
-            
-            
-        </section>
-    </main>
-    
+
+    <aside id="menuLat">
+        <div class="menuLateral">
+            <ul>
+                <!-- <li><a href="#"><i class="fas fa-tv"></i>dashboard</a></li> -->
+                <li class="opL"><a href="#"><i class='fa-solid fa-house'></i></a></li>
+                <li class="opL"><a href="#"><i class="far fa-user"></i></a></li>
+                <!-- <li><a href="#"><i class="fab fa-gripfire"></i>trending</a></li> -->
+                <li class="opL"><a href="#"><i class="far fa-comments"></i></a></li>
+                <!-- <li><a href="#"><i class="far fa-folder"></i>file manager</a></li> -->
+                <!-- <li><a href="#"><i class="far fa-address-book"></i>portfolio</a></li> -->
+                <li class="opL"><a href="#"><i class="fas fa-heart"></i></a></li>
+                <li class="opL"><a href="#"><i class="fas fa-cogs"></i></a></li>
+                <li class="opL"><a href="./inicioSesionForm/cerrarSesion.php"><i class='fas fa-sign-out-alt'></i></a>
+                </li>
+            </ul>
+        </div>
+    </aside>
+    <section id="resultadosPerfiles">
+
+
+    </section>
+
+
     <?php
 
     include "./elementos/footerBlanco.php";
